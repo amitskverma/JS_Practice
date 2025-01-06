c4d5b9920848a6c0ddb4e74dc22fad1b94694045
amitskverma <amitskverma@hotmail.com>
2025-01-06 20:04:49 +0530

    data-types

---
+++ Day1/data_types.js
@@ -0,0 +1,49 @@
+// String - Primitive Data Type
+let name = "John Doe";
+console.log("String:", name);
+
+// Number - Primitive Data Type
+let age = 30;
+console.log("Number:", age);
+
+// Boolean - Primitive Data Type
+let isMarried = true;
+console.log("Boolean:", isMarried);
+
+// Undefined -Primitive Data Type
+let address;
+console.log("Undefined:", address);
+
+// Null -Primitive Data Type
+let car = null;
+console.log("Null:", car);
+
+// Object - Non-Primitive Data Type
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+console.log("Object:", person);
+
+// Array - non-Primitive Data Type
+let colors = ["Red", "Green", "Blue"];
+console.log("Array:", colors);
+
+// Symbol
+let sym = Symbol('foo');
+console.log("Symbol:", sym);
+
+// BigInt
+let bigNumber = BigInt(1234567890123456789012345678901234567890n);
+console.log("BigInt:", bigNumber);
+
+
+// primitive data types:
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+// Symbol
+// BigInt
