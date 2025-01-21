7f76b9ef596708064cf2dccd48f2383d40f638b2
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-01-21 09:31:10 +0530

    more in function

--- Day2/function.js
+++ Day2/function.js
@@ -1,4 +1,31 @@
+
+function demo() {
+  let department = "Engineering";
+    console.log("Hello, World!");
+}
+let department = "Finance";
+demo();
+console.log(department);
+
+
+function sayHello(name) {
+console.log("Hello, " + "  " + name[1]);
+}
+sayHello(["John", "Mike", "Jane"]);
+
 // Function Declaration
+
+function multiply2(a, b) {
+    //var c =5 ;
+    return a * b;
+}
+//console.log(c);
+console.log(multiply2(2,2));
+
+//arrow function    
+const multiply3 = (a, b) => a * b;
+console.log(multiply3(2, 3));
+
 function greet(name) {
     return `Hello, ${name}!`;
 }
@@ -13,3 +40,11 @@ console.log(add(5, 3));
 // Arrow Function
 const multiply = (a, b) => a * b;
 console.log(multiply(4, 2));
+
+
+
+
+
+
+
+
